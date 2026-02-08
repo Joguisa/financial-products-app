ad2ec4ccfe32d02e6a1b363a9fec3336d99c21eb
Joguisa <jntnglln@gmail.com>
2026-02-07 23:33:39 -0500

    feat: implement responsive design and update color palette

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
-import { Component, effect, inject, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FinancialProductsFacade } from './features/financial-products/application/financial-products-facade.service';
 
 @Component({
   selector: 'bp-root',
@@ -9,17 +8,6 @@ import { FinancialProductsFacade } from './features/financial-products/applicati
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
-  private readonly facade = inject(FinancialProductsFacade);
+export class AppComponent {
 
-  constructor() {
-    effect(() => {
-      console.log('Datos en el Estado (Signals):', this.facade.products());
-    });
-  }
-
-  ngOnInit(): void {
-    console.log('Iniciando petición de productos...');
-    this.facade.fetchAllProducts();
-  }
 }
